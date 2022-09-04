e0af03e03715e3ce19f4dc02078234fecea14fc4
Francisco Castellanos <xacfran@gmail.com>
2022-09-04 11:54:12 -0500

    Updated gitignore file and rules

--- helper_functions.R
+++ helper_functions.R
@@ -1,6 +1,7 @@
 #author: matt leary
 #date:11/10/2019
 #intent: helper functions for resume built with R pagedown
+#minor width modifications to the bars
 
 library(dplyr)
 library(ggplot2)
@@ -9,18 +10,17 @@ library(purrr)
 
 # Construct a bar chart of skills
 build_skill_bars <- function(skill_data, section_title) {
-  skill_data %>%  
-    filter(section == section_title) %>% 
+  skill_data %>%
+    filter(section == section_title) %>%
     ggplot(aes(x= reorder(skill, level), y = 5)) +
     geom_col(fill = "lightgrey", width = 0.85) +
     geom_col(aes(x= reorder(skill, level), y = level), fill = "#98D4F0", width = 0.85) +
     coord_flip() +
     geom_text(aes(label = skill,  y = 0.25), hjust = 0, size = 12, color = "white") +
     expand_limits(y = c(0,3)) +
-    labs(x = NULL,
-         y = NULL) + 
+    labs(x = NULL, y = NULL) +
     theme_void( ) +
     theme(panel.background = element_rect(fill = "transparent", colour = NA),
           plot.background  = element_rect(fill = "transparent", colour = NA))
-    
+
 }
